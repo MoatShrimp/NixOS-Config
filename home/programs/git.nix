bf6df43b71f49c05bec50eb6cc3baaa51b77b67f
MoatShrimp <mathias.viklund@gmail.com>
2024-10-10 15:02:18 +0200

    setup git config

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -3,9 +3,27 @@
     enable = true;
     userName = "MoatShrimp";
     userEmail = "mathias.viklund@gmail.com";
+    delta.enable = true;
+    ignores = [
+      "**/.env"
+    ];
+    signing = {
+      signByDefault = true;
+      key = "~/.ssh/id_ed25519.pub";
+    };
     extraConfig = {
-      push = { autoSetupRemote = true; };
-      core = { editor = "code --wait"; };
+      init.defaultBranch = "main";
+      gpg.format = "ssh";
+      push.autoSetupRemote = true;
+      core.editor = "code --wait";
+      merge.conflictstyle = "zdiff3";
+      rerere.enabled = true;
+      diff.algorithm = "histogram";
+      log.abbrevCommit = true;
+      help.autocorrect = "prompt";
+      fetch.prune = true;
+      pull.rebase = true;
+      url."git@github.com:".insteadOf = "https://github.com/";
     };
   };
 }
